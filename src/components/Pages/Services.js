523c87217a194d30246c8c6578639fd641806e8e
Spandana Morampudi <spandana.morampudi01@utrgv.edu>
2024-02-17 16:49:29 -0600

    static design

---
+++ src/components/Pages/Services.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import '../../App.css'
+export default function Services() {
+  return (
+    <div>
+      <h1 className='products'>Services</h1>
+    </div>
+  )
+}
---
+++ src/components/Pages/SignUp.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import '../../App.css'
+export default function SignUp() {
+  return (
+    <div>
+      <h1 className='products'>SIGN UP</h1>
+    </div>
+  )
+}
